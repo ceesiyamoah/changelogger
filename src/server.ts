476a5bb286b8b6aeeb37977a60d3b34d6eb9a543
Cyril <ccyamoah@gmail.com>
2024-02-27 22:22:38 +0000

    prisma migration

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import express from 'express';
+import router from './router';
+import morgan from 'morgan';
 
 const app = express();
 
-app.get('/', (req, res) => {
-	res.status(200);
-	res.json({
-		message: 'works',
-	});
-});
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', router);
 
 export default app;
